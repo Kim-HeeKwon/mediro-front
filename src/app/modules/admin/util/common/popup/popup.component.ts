36fe4d17e58531af33e771be2e156c75c5e07a35
teamplat <tidlsld07@gmail.com>
2021-07-02 22:23:10 +0900

    * 작성자 : 강병규 (팀플랫 PL) 2021.07.02 Version : 0.2 - 공통 팝업 수정

--- src/app/modules/admin/util/common/popup/popup.component.ts
+++ src/app/modules/admin/util/common/popup/popup.component.ts
@@ -1,12 +1,24 @@
-import {Component, Inject, OnInit, ViewChild} from '@angular/core';
+import {
+    AfterViewInit, ChangeDetectionStrategy,
+    ChangeDetectorRef,
+    Component,
+    Inject,
+    OnDestroy,
+    OnInit,
+    ViewChild,
+    ViewEncapsulation
+} from '@angular/core';
 import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
 import {CommonCode, CommonPopup, FuseUtilsService} from "../../../../../../@teamplat/services/utils";
 import {PopupStore} from "../../../../../core/common-popup/state/popup.store";
 import {FormBuilder, FormGroup} from "@angular/forms";
 import {PopupService} from "./popup.service";
-import {BehaviorSubject, Observable} from "rxjs";
-import {AccountData, AccountPagenation} from "../../../basic-info/account/account.types";
-import {PopupPagenation} from "./popup.types";
+import {BehaviorSubject, merge, Observable, Subject} from 'rxjs';
+import {PopupPagenation} from './popup.types';
+import {map, switchMap, takeUntil} from 'rxjs/operators';
+import {MatSort} from '@angular/material/sort';
+import {MatPaginator} from '@angular/material/paginator';
+import {fuseAnimations} from '../../../../../../@teamplat/animations';
 
 export interface DisplayedColumn{
     id: string;
@@ -17,35 +29,20 @@ export interface Column{
     name: string;
 }
 
-/*export interface PeriodicElement {
-    name: string;
-    position: number;
-    weight: number;
-    symbol: string;
-}*/
-/*
-const ELEMENT_DATA: PeriodicElement[] = [
-    {position: 1, name: 'Hydrogen', weight: 100, symbol: 'H'},
-    {position: 2, name: 'Helium', weight: 100, symbol: 'He'},
-    {position: 3, name: 'Lithiumsssssssss', weight: 100.941, symbol: 'Li'},
-    {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-    {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-    {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-    {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-    {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-    {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-];
-*/
-
 @Component({
     selector: 'app-popup',
     templateUrl: './popup.component.html',
-    styleUrls: ['./popup.component.scss']
+    styleUrls: ['./popup.component.scss'],
+    encapsulation  : ViewEncapsulation.None,
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    animations     : fuseAnimations
 })
-export class PopupComponent implements OnInit {
-
+export class PopupComponent implements OnInit, OnDestroy, AfterViewInit {
+    @ViewChild(MatPaginator) private _paginator: MatPaginator;
+    @ViewChild(MatSort) private _sort: MatSort;
     isLoading: boolean = false;
-    displayedColumns: DisplayedColumn[];
+    popupCount: number = 0;
+    displayedColumns: DisplayedColumn[] = [];
     clickedRows = new Set<any>();
     getList$: Observable<any>;
     searchForm: FormGroup;
@@ -53,26 +50,30 @@ export class PopupComponent implements OnInit {
     asPopupCd: string;
     pagenation: PopupPagenation | null = null;
 
+    commonValues: Column[] = [];
+
     accountNm1 = '고객사';
     accountNm2 = '고객사 명';
 
     type: CommonCode[] = [];
 
     private _dataSet: BehaviorSubject<any> = new BehaviorSubject(null);
-
+    private _unsubscribeAll: Subject<any> = new Subject<any>();
     constructor(
         public _matDialogRef: MatDialogRef<PopupComponent>,
         @Inject(MAT_DIALOG_DATA) public data: any,
         private _utilService: FuseUtilsService,
         private _formBuilder: FormBuilder,
         private _popupService: PopupService,
+        private _changeDetectorRef: ChangeDetectorRef,
         private _popupStore: PopupStore) {
         this.popupInfo = _utilService.commonPopupValue(_popupStore.getValue().data, data.popup);
         this.asPopupCd = data.popup;
+
+
     }
 
     ngOnInit(): void {
-
         // 검색 Form 생성
         this.searchForm = this._formBuilder.group({
             searchText: [''],
@@ -80,10 +81,6 @@ export class PopupComponent implements OnInit {
             asPopupCd: [''],
             acWhereVal: ['']
         });
-        this.getList$ = null;
-
-        const commonValues: Column[] = [];
-        const displayCommonValue: DisplayedColumn[] = [];
         this.popupInfo.forEach((param: any) => {
             const commonValue: Column = {
                 id: param.extColId,              //컬럼ID;
@@ -98,25 +95,88 @@ export class PopupComponent implements OnInit {
                 this.searchForm.patchValue({'type' : param.extColId});
             }
 
-            displayCommonValue.push(param.extColId);
-            commonValues.push(commonValue);
+            this.displayedColumns.push(param.extColId);
+            this.commonValues.push(commonValue);
 
         });
 
-        this.displayedColumns = displayCommonValue;
+        // getList$
+        this.getList$ = this._popupService.getList$;
+        this._popupService.getList$
+            .pipe(takeUntil(this._unsubscribeAll))
+            .subscribe((getList: any) => {
+                // Update the counts
+                if(getList !== null){
+                    this.popupCount = getList.length;
+                }
+
+                // Mark for check
+                this._changeDetectorRef.markForCheck();
+            });
+
+        // Get the pagenation
+        this._popupService.pagenation$
+            .pipe(takeUntil(this._unsubscribeAll))
+            .subscribe((pagenation: PopupPagenation) => {
+                // Update the pagination
+                this.pagenation = pagenation;
+                // Mark for check
+                this._changeDetectorRef.markForCheck();
+            });
 
     }
 
-    selectRow(row: any): void{
+
+    /**
+     * After view init
+     */
+    ngAfterViewInit(): void {
+        console.log(this._sort);
+        if(this._sort !== undefined){
+            // Get products if sort or page changes
+            merge(this._sort.sortChange, this._paginator.page).pipe(
+                switchMap(() => {
+                    this.searchForm.patchValue({'asPopupCd': this.asPopupCd});
+                    this.searchForm.patchValue({'acWhereVal': 'K:LIKE_BOTH:'});
+                    this.isLoading = true;
+                    // eslint-disable-next-line max-len
+                    return this._popupService.getDynamicSql(this._paginator.pageIndex, this._paginator.pageSize, this._sort.active, this._sort.direction, this.searchForm.getRawValue());
+                }),
+                map(() => {
+                    this.isLoading = false;
+                })
+            ).subscribe();
+        }
+    }
+
+    /**
+     * On destroy
+     */
+    ngOnDestroy(): void {
+        // Unsubscribe from all subscriptions
+        this._unsubscribeAll.next();
+        this._unsubscribeAll.complete();
+    }
+
+    /**
+     * Track by function for ngFor loops
+     *
+     * @param index
+     * @param account
+     */
+    trackByFn(index: number, data: any): any {
+        //console.log(data);
+        return data.id || index;
+    }
+
+    selectRow(row: any): void {
         this._matDialogRef.close(row);
     }
-
     select(): void{
         this.searchForm.patchValue({'asPopupCd': this.asPopupCd});
         this.searchForm.patchValue({'acWhereVal': 'K:LIKE_BOTH:'});
-        this._popupService.getDynamicSql(0,10,'account','asc',this.searchForm.getRawValue());
+        this._popupService.getDynamicSql(0,10,'accountCd','asc',this.searchForm.getRawValue());
 
-        // getList
-        this.getList$ = this._popupService.getList$;
+
     }
 }
--- src/app/modules/admin/util/common/popup/popup.service.ts
+++ src/app/modules/admin/util/common/popup/popup.service.ts
@@ -2,32 +2,19 @@ import {Injectable} from "@angular/core";
 import {BehaviorSubject, Observable} from "rxjs";
 import {HttpClient} from "@angular/common/http";
 import {Common} from "../../../../../../@teamplat/providers/common/common";
+import {PopupPagenation} from "./popup.types";
 
 @Injectable({
     providedIn: 'root'
 })
 export class PopupService{
     private _getList: BehaviorSubject<any> = new BehaviorSubject(null);
-
+    private _pagenation: BehaviorSubject<PopupPagenation | null> = new BehaviorSubject(null);
     /**
      * Constructor
      */
     constructor(private _httpClient: HttpClient, private _common: Common) {
     }
-    getDynamicSql(page: number = 0, size: number = 10, sort: string = '', order: 'asc' | 'desc' | '' = 'asc', search: any = {}):
-        Observable<any>{
-        // @ts-ignore
-        return new Promise((resolve, reject) => {
-            this._common.sendData(search, 'v1/api/common/popup/module-query')
-                .subscribe((response: any) => {
-                    if(response.status === 'SUCCESS'){
-                        this._getList.next(response.data);
-                        resolve(this._getList);
-                    }
-                }, reject);
-        });
-    }
-
     /**
      * Getter
      */
@@ -35,4 +22,45 @@ export class PopupService{
     {
         return this._getList.asObservable();
     }
+
+    /**
+     * Getter for pagenation
+     */
+    get pagenation$(): Observable<PopupPagenation>
+    {
+        return this._pagenation.asObservable();
+    }
+    getDynamicSql(page: number = 0, size: number = 10, sort: string = 'account', order: 'asc' | 'desc' | '' = 'asc', search: any = {}):
+        Observable<{ pagenation: PopupPagenation; getList: any }>{
+
+        const searchParam = {};
+        searchParam['order'] = order;
+        searchParam['sort'] = sort;
+
+        // 검색조건 Null Check
+        if((Object.keys(search).length === 0) === false){
+            // eslint-disable-next-line guard-for-in
+            for (const k in search) {
+                searchParam[k] = search[k];
+            }
+        }
+        console.log('getDynamicSql');
+
+        const pageParam = {
+            page: page,
+            size: size,
+        };
+        // @ts-ignore
+        return new Promise((resolve, reject) => {
+            this._common.sendDataWithPageNation(searchParam, pageParam, 'v1/api/common/popup/module-query')
+                .subscribe((response: any) => {
+                    if(response.status === 'SUCCESS'){
+                        this._getList.next(response.data);
+                        this._pagenation.next(response.pageNation);
+                        resolve(this._getList);
+                    }
+                }, reject);
+        });
+    }
+
 }
