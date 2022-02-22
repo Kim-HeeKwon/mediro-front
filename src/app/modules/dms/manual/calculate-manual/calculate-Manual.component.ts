f70eb15504011030eed98a1ddc1cd099a626c036
kimjinyoung98 <0623jia@team-plat.com>
2022-02-22 19:06:18 +0900

    * 작성자 : 김진영 (팀플랫 PM) 2022.02.22 Version : 0.1 - 정산 관리 메뉴얼 추가

--- src/app/modules/dms/manual/calculate-manual/calculate-Manual.component.ts
+++ src/app/modules/dms/manual/calculate-manual/calculate-Manual.component.ts
@@ -11,12 +11,13 @@ import {DeviceDetectorService} from "ngx-device-detector";
 
 // eslint-disable-next-line @typescript-eslint/naming-convention
 export class calculateManualComponent {
+    url: SafeResourceUrl;
+    video: boolean = false;
     bill: boolean = false;
     tax: boolean = false;
     deposit: boolean = false;
     withdrawal: boolean = false;
     incomeoutcome: boolean = false;
-    url: SafeResourceUrl;
     isMobile: boolean = false;
 
     constructor(public sanitizer: DomSanitizer,
@@ -83,4 +84,101 @@ export class calculateManualComponent {
             this.incomeoutcome = true;
         }
     }
+
+    billLookUp(): void {
+        this.video = true;
+        // eslint-disable-next-line max-len
+        this.url = this.sanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/1QahfomcobQ');
+    }
+
+    billConfirmed(): void {
+        this.video = true;
+        // eslint-disable-next-line max-len
+        this.url = this.sanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/JKrNTobVdbs');
+    }
+
+    billUpdate(): void {
+        this.video = true;
+        // eslint-disable-next-line max-len
+        this.url = this.sanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/Ryjf-grDDpM');
+    }
+
+    taxLookUp(): void {
+        this.video = true;
+        // eslint-disable-next-line max-len
+        this.url = this.sanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/Or0JUAEgazw');
+    }
+
+    taxDelet(): void {
+        this.video = true;
+        // eslint-disable-next-line max-len
+        this.url = this.sanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/afubjV8iN4Q');
+    }
+
+    depositLookUp(): void {
+        this.video = true;
+        // eslint-disable-next-line max-len
+        this.url = this.sanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/uCHjavWCCJE');
+    }
+
+    depositCreat(): void {
+        this.video = true;
+        // eslint-disable-next-line max-len
+        this.url = this.sanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/2su2iQP-TUU');
+    }
+
+    depositDelet(): void {
+        this.video = true;
+        // eslint-disable-next-line max-len
+        this.url = this.sanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/ZEmOSPuGiJ4');
+    }
+
+    depositConfirmed(): void {
+        this.video = true;
+        // eslint-disable-next-line max-len
+        this.url = this.sanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/3t6Rnjuv8Ow');
+    }
+
+    withdrawalLookUp(): void {
+        this.video = true;
+        // eslint-disable-next-line max-len
+        this.url = this.sanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/mj-EmHCWMuY');
+    }
+
+    withdrawalCreat(): void {
+        this.video = true;
+        // eslint-disable-next-line max-len
+        this.url = this.sanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/HWbCbpS0560');
+    }
+
+    withdrawalDelet(): void {
+        this.video = true;
+        // eslint-disable-next-line max-len
+        this.url = this.sanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/SpWgUrhHWr4');
+    }
+
+    withdrawalConfirmed(): void {
+        this.video = true;
+        // eslint-disable-next-line max-len
+        this.url = this.sanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/bhBDxAq6jto');
+    }
+
+    incomeoutcomeLookUp(): void {
+        this.video = true;
+        // eslint-disable-next-line max-len
+        this.url = this.sanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/k3h_zjoEUy0');
+    }
+
+    incomeoutcomeTotalLookUp(): void {
+        this.video = true;
+        // eslint-disable-next-line max-len
+        this.url = this.sanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/XRLxnYtI5YI');
+    }
+
+    incomeoutcomeTotal(): void {
+        this.video = true;
+        // eslint-disable-next-line max-len
+        this.url = this.sanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/TWdPUvYCNJU');
+    }
+
 }
